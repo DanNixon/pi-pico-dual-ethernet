3343908051704b1762be504a854c56f57a98f973
Dan Nixon <dan@dan-nixon.com>
2026-02-25 20:37:34 +0000

    Tidy up and add features to demo firmware

--- demo-firmware/src/main.rs
+++ demo-firmware/src/main.rs
@@ -1,27 +1,24 @@
 #![no_std]
 #![no_main]
 
+mod buttons;
+mod display;
+mod network;
+mod wall_time;
+
 use defmt::{info, warn};
 use defmt_rtt as _;
-use embassy_embedded_hal::shared_bus::asynch::spi::SpiDeviceWithConfig;
 use embassy_executor::Spawner;
-use embassy_futures::yield_now;
-use embassy_net::{Config, Stack, StackResources};
-use embassy_net_wiznet::{Device, Runner, State, chip::W5500};
-use embassy_rp::{
-    Peri, bind_interrupts,
-    clocks::RoscRng,
-    gpio::{Input, Level, Output, Pull},
-    peripherals::{self, I2C1, SPI0, SPI1},
-    spi::Spi,
+use embassy_net::Stack;
+use embassy_rp::{Peri, peripherals};
+use embassy_sync::{
+    blocking_mutex::raw::CriticalSectionRawMutex, channel::Channel, pubsub::WaitResult,
 };
-use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, channel::Channel, mutex::Mutex};
 use embassy_time::{Duration, Timer};
 use embedded_io_async::Write;
 use heapless::Vec;
 use panic_probe as _;
 use portable_atomic as _;
-use static_cell::StaticCell;
 
 assign_resources::assign_resources! {
     // Ethernet 1 is the same pins as the WIZnet evaluation boards
@@ -47,8 +44,8 @@ assign_resources::assign_resources! {
         int_pin: PIN_13,
         rst_pin: PIN_12,
     },
-    i2c: I2cResources {
-        peripheral: I2C1,
+    display: DisplayResources {
+        i2c: I2C1,
         sda: PIN_14,
         scl: PIN_15,
     },
@@ -59,29 +56,21 @@ assign_resources::assign_resources! {
     },
 }
 
-bind_interrupts!(struct Irqs {
-    I2C1_IRQ => embassy_rp::i2c::InterruptHandler<I2C1>;
-});
-
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
     let r = split_resources!(p);
 
-    info!("Hello, world!");
+    spawner.must_spawn(display::task(r.display));
 
-    let _i2c = embassy_rp::i2c::I2c::new_async(
-        r.i2c.peripheral,
-        r.i2c.scl,
-        r.i2c.sda,
-        Irqs,
-        Default::default(),
-    );
+    spawner.must_spawn(say_button_task());
+    buttons::init(r.buttons, spawner);
 
-    spawner.must_spawn(button_task(r.buttons));
+    let net_stack_1 = network::init_ethernet_1(r.ethernet_1, spawner).await;
+    let net_stack_2 = network::init_ethernet_2(r.ethernet_2, spawner).await;
 
-    let net_stack_1 = init_ethernet_1(r.ethernet_1, spawner).await;
-    let net_stack_2 = init_ethernet_2(r.ethernet_2, spawner).await;
+    spawner.must_spawn(say_time_task());
+    spawner.must_spawn(wall_time::ntp_task(net_stack_2));
 
     spawner.must_spawn(listen_task(net_stack_1, 0, 1234));
     spawner.must_spawn(listen_task(net_stack_1, 1, 1234));
@@ -90,172 +79,33 @@ async fn main(spawner: Spawner) {
 }
 
 #[embassy_executor::task]
-async fn button_task(r: ButtonResources) -> ! {
-    let btn_1 = Input::new(r.user_1, Pull::Up);
-    let btn_2 = Input::new(r.user_2, Pull::Up);
-    let btn_3 = Input::new(r.user_3, Pull::Up);
+async fn say_button_task() -> ! {
+    let mut subscriber = buttons::BUTTON_EVENTS.subscriber().unwrap();
 
     loop {
-        info!(
-            "Button 1/2/3: {}/{}/{}",
-            btn_1.get_level(),
-            btn_2.get_level(),
-            btn_3.get_level(),
-        );
-
-        Timer::after_secs(1).await;
+        match subscriber.next_message().await {
+            WaitResult::Lagged(_) => {
+                unreachable!();
+            }
+            WaitResult::Message(event) => {
+                info!("Button: {}", event);
+            }
+        }
     }
 }
 
-fn w5500_spi_config() -> embassy_rp::spi::Config {
-    let mut spi_config = embassy_rp::spi::Config::default();
-    spi_config.frequency = 50_000_000;
-    spi_config.phase = embassy_rp::spi::Phase::CaptureOnSecondTransition;
-    spi_config.polarity = embassy_rp::spi::Polarity::IdleHigh;
-    spi_config
-}
-
-async fn init_ethernet_1(r: crate::Ethernet1Resources, spawner: Spawner) -> Stack<'static> {
-    let spi = Spi::new(
-        r.spi,
-        r.clk,
-        r.mosi,
-        r.miso,
-        r.tx_dma,
-        r.rx_dma,
-        w5500_spi_config(),
-    );
-
-    static SPI: StaticCell<
-        Mutex<CriticalSectionRawMutex, Spi<'static, SPI0, embassy_rp::spi::Async>>,
-    > = StaticCell::new();
-    let spi = SPI.init(Mutex::new(spi));
-
-    let cs = Output::new(r.cs_pin, Level::High);
-    let device = SpiDeviceWithConfig::new(spi, cs, w5500_spi_config());
-
-    let w5500_int = Input::new(r.int_pin, Pull::Up);
-    let w5500_reset = Output::new(r.rst_pin, Level::High);
-
-    let mac_addr = [0x02, 0x00, 0x00, 0x00, 0x55, 0x01];
-
-    static STATE: StaticCell<State<8, 8>> = StaticCell::new();
-    let state = STATE.init(State::<8, 8>::new());
-
-    let (device, runner) = embassy_net_wiznet::new(mac_addr, state, device, w5500_int, w5500_reset)
-        .await
-        .unwrap();
-
-    spawner.must_spawn(ethernet_1_task(runner));
-
-    static RESOURCES: StaticCell<StackResources<8>> = StaticCell::new();
-    let mut rng = RoscRng;
-    let (stack, runner) = embassy_net::new(
-        device,
-        Config::dhcpv4(Default::default()),
-        RESOURCES.init(StackResources::new()),
-        rng.next_u64(),
-    );
-    spawner.must_spawn(net_task(runner));
-
-    info!("Waiting for DHCP...");
-    let cfg = wait_for_config(stack).await;
-    let local_addr = cfg.address.address();
-    info!("IP address: {:?}", local_addr);
-
-    stack
-}
-
-async fn init_ethernet_2(r: crate::Ethernet2Resources, spawner: Spawner) -> Stack<'static> {
-    let spi = Spi::new(
-        r.spi,
-        r.clk,
-        r.mosi,
-        r.miso,
-        r.tx_dma,
-        r.rx_dma,
-        w5500_spi_config(),
-    );
-
-    static SPI: StaticCell<
-        Mutex<CriticalSectionRawMutex, Spi<'static, SPI1, embassy_rp::spi::Async>>,
-    > = StaticCell::new();
-    let spi = SPI.init(Mutex::new(spi));
-
-    let cs = Output::new(r.cs_pin, Level::High);
-    let device = SpiDeviceWithConfig::new(spi, cs, w5500_spi_config());
-
-    let w5500_int = Input::new(r.int_pin, Pull::Up);
-    let w5500_reset = Output::new(r.rst_pin, Level::High);
-
-    let mac_addr = [0x02, 0x00, 0x00, 0x00, 0x55, 0x02];
-
-    static STATE: StaticCell<State<8, 8>> = StaticCell::new();
-    let state = STATE.init(State::<8, 8>::new());
-
-    let (device, runner) = embassy_net_wiznet::new(mac_addr, state, device, w5500_int, w5500_reset)
-        .await
-        .unwrap();
-
-    spawner.must_spawn(ethernet_2_task(runner));
-
-    static RESOURCES: StaticCell<StackResources<8>> = StaticCell::new();
-    let mut rng = RoscRng;
-    let (stack, runner) = embassy_net::new(
-        device,
-        Config::dhcpv4(Default::default()),
-        RESOURCES.init(StackResources::new()),
-        rng.next_u64(),
-    );
-    spawner.must_spawn(net_task(runner));
-
-    info!("Waiting for DHCP...");
-    let cfg = wait_for_config(stack).await;
-    let local_addr = cfg.address.address();
-    info!("IP address: {:?}", local_addr);
-
-    stack
-}
-
-type EthernetSpi<SPI> = SpiDeviceWithConfig<
-    'static,
-    CriticalSectionRawMutex,
-    Spi<'static, SPI, embassy_rp::spi::Async>,
-    Output<'static>,
->;
-
 #[embassy_executor::task]
-async fn ethernet_1_task(
-    runner: Runner<'static, W5500, EthernetSpi<SPI0>, Input<'static>, Output<'static>>,
-) -> ! {
-    runner.run().await
-}
-
-#[embassy_executor::task]
-async fn ethernet_2_task(
-    runner: Runner<'static, W5500, EthernetSpi<SPI1>, Input<'static>, Output<'static>>,
-) -> ! {
-    runner.run().await
-}
-
-#[embassy_executor::task(pool_size = 2)]
-async fn net_task(mut runner: embassy_net::Runner<'static, Device<'static>>) -> ! {
-    runner.run().await
-}
-
-async fn wait_for_config(stack: Stack<'static>) -> embassy_net::StaticConfigV4 {
+async fn say_time_task() -> ! {
     loop {
-        if let Some(config) = stack.config_v4() {
-            return config.clone();
-        }
-        yield_now().await;
+        info!("Wall clock: {}", wall_time::now());
+        Timer::after_secs(1).await;
     }
 }
 
 static CHANNEL: Channel<CriticalSectionRawMutex, Vec<u8, 256>, 8> = Channel::new();
 
 #[embassy_executor::task(pool_size = 2)]
-async fn listen_task(stack: Stack<'static>, id: u8, port: u16) {
+async fn listen_task(stack: Stack<'static>, id: u8, port: u16) -> ! {
     let mut rx_buffer = [0; 4096];
     let mut tx_buffer = [0; 4096];
     let mut buf = [0; 4096];
@@ -301,7 +151,7 @@ async fn listen_task(stack: Stack<'static>, id: u8, port: u16) {
 }
 
 #[embassy_executor::task]
-async fn repeat_task(stack: Stack<'static>, port: u16) {
+async fn repeat_task(stack: Stack<'static>, port: u16) -> ! {
     let mut rx_buffer = [0; 4096];
     let mut tx_buffer = [0; 4096];
 
